04ba7ffa1c3abe6182dbd4ed7899bff63eeb2fbe
Max Carlquist <tenkaklet@gmail.com>
2019-03-27 21:54:50 +0100

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SourcesComponent } from './sources/sources.component';
+import { ResultsComponent } from './results/results.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SourcesComponent,
+    ResultsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
